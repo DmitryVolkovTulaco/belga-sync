8cc365edce5e2088aab84df586d8b65d62343896
Alexander Trauzzi <acj@trauzzi.me>
2020-03-03 11:51:00 -0600

    Make prezly newsroom required for now, quick doc.

--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
 # Prezly Belga Sync
+
+### Running
+
+To run an instance of the container, run with a completed copy of `.env.example` to `/source/.env`, as follows:
+
+```
+docker run --volume ./.env:/source/.env belga-sync -- <belga_board_uuid> <prezly_newsroom_id> [belga_offset]
+```
--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,6 @@ log4js.configure({
 
 const vorpal = new Vorpal();
 
-vorpal.command('import <belga_board_uuid> [prezly_newsroom_id] [belga_offset]').action(belgaImport);
+vorpal.command('import <belga_board_uuid> <prezly_newsroom_id> [belga_offset]').action(belgaImport);
 
 vorpal.parse(process.argv);
