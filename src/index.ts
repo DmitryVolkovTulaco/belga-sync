8dc76b4f3e159ef1b5c98777cfbf9c2f6e4cf86f
Alexander Trauzzi <acj@trauzzi.me>
2020-03-05 10:18:58 -0600

    Introduce a wrapper for commands
    - The wrapper  will trap all exceptions and route them through our logging stack.
    - I've chosen to use fatal so that they stand out. Even though the process will continue as a result of this.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import 'cross-fetch/polyfill';
 import log4js from 'log4js';
-import Vorpal from 'vorpal';
+import Vorpal, { Args } from 'vorpal';
 import { belgaImport } from './commands/belga-import';
 import dotenv from 'dotenv';
 import sentryExporter from './util/sentry';
@@ -25,8 +25,20 @@ log4js.configure({
     },
 });
 
+const logger = log4js.getLogger();
+
 const vorpal = new Vorpal();
 
-vorpal.command('import <belga_board_uuid> <prezly_newsroom_id> [belga_offset]').action(belgaImport);
+vorpal.command('import <belga_board_uuid> <prezly_newsroom_id> [belga_offset]').action(wrapCommand(belgaImport));
 
 vorpal.parse(process.argv);
+
+function wrapCommand<ArgsType extends Args>(command: (args: ArgsType) => Promise<any>) {
+    return async (args: ArgsType) => {
+        try {
+            await command(args);
+        } catch (error) {
+            logger.fatal(`Unhandled exception.`, [JSON.stringify(error, null, 4)]);
+        }
+    };
+}
